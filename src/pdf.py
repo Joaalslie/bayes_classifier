9613aaf976fa64bcc8b436fa8ff55034367ffba6
joaalslie <jal029@uit.no>
2020-12-09 18:34:23 +0100

    Add skeleton code for probability density functions

---
+++ src/pdf.py
@@ -0,0 +1,59 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+
+class PDF(ABC):
+    """
+        An abstract base class acting as parent-class for probability density
+        functions. The main aim of the class is to ensure that classes
+        inheriting it have implemented a pdf-method.
+    """
+    @abstractmethod
+    def pdf(self):
+        pass
+
+
+class multivariate_normal(PDF):
+    """
+        A class which represents the multivariate normal distribution.
+    """
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def pdf(self):
+        """
+
+        """
+        pass
+
+    def log_pdf(self):
+        """
+
+        """
+        pass
+
+
+class normal(PDF):
+    """
+        A class which represents the singlevariate normal distribution.
+    """
+    def __init__(self):
+        """
+
+        """
+        pass
+
+    def pdf(self):
+        """
+
+        """
+        pass
+
+    def log_pdf(self):
+        """
+
+        """
+        pass
