c42e3871e5e980adb7b0a7c520f4753e69adc63f
joaalslie <jal029@uit.no>
2020-12-09 18:34:35 +0100

    Add skeleton for classifier

---
+++ src/bayes_classifier.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class BayesClassifier():
+    """
+
+    """
+    def __init__(self, num_classes):
+        """
+
+        """
+        self.num_classes = num_classes
+        self.is_fit = False
+
+    def fit(self):
+        """
+
+        """
+        pass
+
+    def predict(self):
+        """
+
+        """
+        pass
