a3d113a5bac5c75b083a296f2e00c5f337040894
joaalslie <jal029@uit.no>
2020-12-10 22:16:34 +0100

    Add some attributes and write prediction function

--- src/bayes_classifier.py
+++ src/bayes_classifier.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from pdf import PDF
+
 
 class BayesClassifier():
     """
@@ -10,7 +12,10 @@ class BayesClassifier():
 
         """
         self.num_classes = num_classes
+        self.added_classes = 0
         self.is_fit = False
+
+        self.distributions = [[] for _ in range(num_classes)]
 
     def fit(self):
         """
@@ -22,4 +27,16 @@ class BayesClassifier():
         """
 
         """
-        pass
+        if self.is_fit:
+            predictions = []
+            # Iterate over each distribution and make prediction
+            for distribution in self.distributions:
+                prediction = distribution.predict()
+                predictions.append(prediction)
+            
+            # Convert prediction list to numpy array
+            predictions = np.array(predictions)
+            # Return the class index with the biggest value
+            return np.argmax(predictions)
+        else:
+            raise Exception("Model has not been trained yet!")
