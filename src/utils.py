183e433a15dc534b8a689bc9e2d19535bd219d60
joaalslie <jal029@uit.no>
2020-12-16 01:43:41 +0100

    Add check for labels in dataset and number of classes in split-function

--- src/utils.py
+++ src/utils.py
@@ -7,8 +7,12 @@ def split_dataset(data, labels, num_classes):
         """
         split_data = [[] for _ in range(num_classes)]
         for datapoint, label in zip(data, labels):
+            if label >= num_classes:
+                raise Exception("number of labels in dataset doesn't " + \
+                    "correlate with the number of classes!")
+
             split_data[int(label)].append(datapoint)
-        
+
         for i in range(num_classes):
             split_data[i] = np.array(split_data[i])
 
