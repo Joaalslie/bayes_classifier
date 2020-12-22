1e7d7a861620ac602bfcdd9f350bea0d3c1edbde
joaalslie <jal029@uit.no>
2020-12-22 21:20:52 +0100

    Add test classes for distributions

---
+++ src/test.py
@@ -0,0 +1,88 @@
+import unittest
+
+
+class TestMultivariateNormal(unittest.TestCase):
+    """
+
+    """
+    def test_pdf(self):
+        """
+
+        """
+        pass
+
+    def test_pdf_exception(self):
+        """
+
+        """
+        pass
+    
+    def test_log_pdf(self):
+        """
+
+        """
+        pass
+    
+    def test_log_pdf_exception(self):
+        """
+
+        """
+        pass
+
+
+class TestNormal(unittest.TestCase):
+    """
+
+    """
+    def test_pdf(self):
+        """
+
+        """
+        pass
+
+    def test_pdf_exception(self):
+        """
+
+        """
+        pass
+    
+    def test_log_pdf(self):
+        """
+
+        """
+        pass
+    
+    def test_log_pdf_exception(self):
+        """
+
+        """
+        pass
+
+
+class TestPoisson(unittest.TestCase):
+    """
+
+    """
+    def test_pdf(self):
+        """
+
+        """
+        pass
+    
+    def test_pdf_exception(self):
+        """
+
+        """
+        pass
+    
+    def test_log_pdf(self):
+        """
+
+        """
+        pass
+    
+    def test_log_pdf_exception(self):
+        """
+
+        """
+        pass
