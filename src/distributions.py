e71423553e54c7a5bf4c536d4a5e7ee8b66134bd
joaalslie <jal029@uit.no>
2021-01-06 22:17:32 +0100

    fill gamma distribution class, add documentation, fix typo

--- src/distributions.py
+++ src/distributions.py
@@ -201,7 +201,7 @@ class Poisson(SingleVariateDistribution):
         Compute singlevariate poisson log pdf based on given x value.
 
         :param x: parameter value for poisson log pdf
-        :returns: the result of the poisson pdf
+        :returns: the result of the poisson log pdf
         :raises Exception: Maximum likelihood estimators have not been set
         """
         if self.is_fit:
@@ -230,27 +230,53 @@ class Gamma(SingleVariateDistribution):
     """
     def __init__(self):
         self.is_fit = False
+        # Gamma function: assume alpha is a positive integer
+        self.gamma = lambda alpha: math.factorial(alpha - 1)
+
+        self.alpha = None
+        self.beta = None
 
     def pdf(self, x):
         """
+        Compute singlevariate gamma log pdf based on given x value.
 
+        :param x: parameter value for gamma pdf
+        :returns: the result of the gamma pdf
+        :raises Exception: Maximum likelihood estimators have not been set
         """
         if self.is_fit:
-            pass
+            return (1 / np.power(self.beta, self.alpha) * \
+                self.gamma(self.alpha)) * \
+                np.power(x, self.alpha - 1) * \
+                np.power(np.e, -x / self.beta)
         else:
             raise Exception("Distribution doesn't have all parameters set!")
 
     def log_pdf(self, x):
         """
+        Compute singlevariate gamma log pdf based on given x value.
 
+        :param x: parameter value for gamma log pdf
+        :returns: the result of the gamma log pdf
+        :raises Exception: Maximum likelihood estimators have not been set
         """
         if self.is_fit:
-            pass
+            return (self.alpha - 1) * np.log(x) - (x / self.beta) - \
+                np.log(self.gamma(self.alpha)) + \
+                (self.alpha * (np.log(self.beta)))
         else:
             raise Exception("Distribution doesn't have all parameters set!")
 
     def fit(self, x):
         """
+        Compute maximum likelihood estimators of the gamma distribution.
 
+        :param data: data used to compute estimators
+        :returns: None
+        :raises Exception: alpha is not a positive integer!
         """
+        if not isinstance(self.alpha, int) and self.alpha < 0:
+            raise Exception("Alpha needs to be set as a positive integer!")
+
+        self.beta = np.mean(x) / self.alpha
         self.is_fit = True
