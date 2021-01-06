c111ed65569f36a4f156d1425489314c9840c8d1
joaalslie <jal029@uit.no>
2021-01-06 20:32:20 +0100

    Remove empty docstrings

--- src/test.py
+++ src/test.py
@@ -4,100 +4,55 @@ import distributions as dists
 
 
 class TestMultivariateNormal(unittest.TestCase):
-    """
-
-    """
     def test_pdf(self):
-        """
-
-        """
         pass
 
     def test_pdf_exception(self):
-        """
-
-        """
         dist = dists.MultivariateNormal()
         # Call on pdf before training to invoke exception
         self.assertRaises(Exception, dist.pdf, None)
 
     
     def test_log_pdf(self):
-        """
-
-        """
         pass
     
     def test_log_pdf_exception(self):
-        """
-
-        """
         dist = dists.MultivariateNormal()
         # Call on logarithmic pdf before training to invoke exception
         self.assertRaises(Exception, dist.log_pdf, None)
 
 
 class TestNormal(unittest.TestCase):
-    """
-
-    """
     def test_pdf(self):
-        """
-
-        """
         pass
 
     def test_pdf_exception(self):
-        """
-
-        """
         dist = dists.Normal()
         # Call on pdf before training to invoke exception
         self.assertRaises(Exception, dist.pdf, None)
     
     def test_log_pdf(self):
-        """
-
-        """
         pass
     
     def test_log_pdf_exception(self):
-        """
-
-        """
         dist = dists.Normal()
         # Call on logarithmic pdf before training to invoke exception
         self.assertRaises(Exception, dist.log_pdf, None)
 
 
 class TestPoisson(unittest.TestCase):
-    """
-
-    """
     def test_pdf(self):
-        """
-
-        """
         pass
     
     def test_pdf_exception(self):
-        """
-
-        """
         dist = dists.Poisson()
         # Call on pdf before training to invoke exception
         self.assertRaises(Exception, dist.pdf, None)
     
     def test_log_pdf(self):
-        """
-
-        """
         pass
     
     def test_log_pdf_exception(self):
-        """
-
-        """
         dist = dists.Poisson()
         # Call on logarithmic pdf before training to invoke exception
         self.assertRaises(Exception, dist.log_pdf, None)
