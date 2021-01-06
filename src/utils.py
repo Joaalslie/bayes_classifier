03e725b5126cc2fffa748031ca80ed4496581b42
joaalslie <jal029@uit.no>
2021-01-06 20:43:34 +0100

    Fill docstring of split_dataset() function

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,13 @@ import numpy as np
 
 def split_dataset(data, labels, num_classes):
         """
+        Split dataset into smaller sets based on corresponding labels.
 
+        :param data: list of feature vectors (dataset)
+        :param labels: list of labels corresponding to the data
+        :param num_classes: number of classes in the dataset
+        :returns: list of split datasets with index corresponding to label
+        :raises Exception: number of labels isn't equal to actual labels
         """
         split_data = [[] for _ in range(num_classes)]
         for datapoint, label in zip(data, labels):
