83ba4705deef9804ae96a1da45624707cf1c466d
joaalslie <jal029@uit.no>
2021-01-17 21:20:49 +0100

    Complete exception tests

--- src/distribution/gamma/test_gamma.py
+++ src/distribution/gamma/test_gamma.py
@@ -8,16 +8,29 @@ class TestGamma(unittest.TestCase):
         pass
 
     def test_pdf_exception(self):
-        pass
+        dist = Gamma()
+        # Call on pdf before training to invoke exception
+        self.assertRaises(Exception, dist.log_pdf, None)
 
     def test_log_pdf(self):
         pass
 
     def test_log_pdf_exception(self):
-        pass
-
-    def test_fit_exceptions(self):
-        pass
+        dist = Gamma()
+        # Call on logarithmic pdf before training to invoke exception
+        self.assertRaises(Exception, dist.log_pdf, None)
 
     def test_set_estimators(self):
         pass
+
+    def test_set_estimators_exception_negative_int(self):
+        dist = Gamma()
+        # Ensure that exception is raised when using negative integer as
+        # parameter in the set_estimators() function
+        self.assertRaises(Exception, dist.set_estimators, -1)
+    
+    def test_set_estimators_exception_non_int(self):
+        dist = Gamma()
+        # Ensure that exception is raised when using negative integer as
+        # parameter in the set_estimators() function
+        self.assertRaises(Exception, dist.set_estimators, 2.34)
