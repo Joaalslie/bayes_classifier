486c9d8c391c26d3b10646da789d3267e6477623
joaalslie <jal029@uit.no>
2021-01-20 22:46:40 +0100

    Add some basic tests for fit() function

--- src/distribution/normal/test_normal.py
+++ src/distribution/normal/test_normal.py
@@ -43,3 +43,23 @@ class TestNormal(unittest.TestCase):
         dist = Normal()
         # Call on logarithmic pdf before training to invoke exception
         self.assertRaises(Exception, dist.log_pdf, None)
+
+    def test_fit_mu(self):
+        # Ensure that mean is correctly set after fit
+        dist = Normal()
+        data = np.array([-4, -3, -2, 2, 3, 4])
+        dist.fit(data)
+        assert dist.mu == 0
+    
+    def test_fit_sigma(self):
+        # Ensure that variance is correctly set after fit
+        dist = Normal()
+        data = np.array([-5, -4, -3, -2, 2, 3, 4, 5])
+        dist.fit(data)
+        assert dist.sigma == 13.5
+    
+    def test_fit_is_fit(self):
+        # Ensure that is_fit bool is correctly set after fit
+        dist = Normal()
+        dist.fit(np.array([0]))
+        assert dist.is_fit == True
