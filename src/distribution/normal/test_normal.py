62c1daeb54f497cc87fbaf28ca6ee41fb5902137
joaalslie <jal029@uit.no>
2021-01-20 22:37:16 +0100

    Add and fill some tests for normal distribution, still need more

--- src/distribution/normal/test_normal.py
+++ src/distribution/normal/test_normal.py
@@ -1,11 +1,28 @@
+import numpy as np
 import unittest
 
 from .normal import Normal
 
 
 class TestNormal(unittest.TestCase):
+    def create_pdf(self, mu, sigma):
+        dist = Normal()
+        # Set the parameters manually for simplicity
+        dist.mu = mu
+        dist.sigma = sigma
+        dist.is_fit = True
+        return dist
+
     def test_pdf(self):
-        pass
+        dist = self.create_pdf(0, 1)
+        y = dist.pdf(1)
+        # 2 decimals should be sufficient
+        assert np.round(y, 2) == 0.24
+
+    def test_pdf_negative_x(self):
+        dist = self.create_pdf(0, 1)
+        y = dist.pdf(-1)
+        assert np.round(y, 2) == 0.24
 
     def test_pdf_exception(self):
         dist = Normal()
@@ -13,7 +30,14 @@ class TestNormal(unittest.TestCase):
         self.assertRaises(Exception, dist.pdf, None)
 
     def test_log_pdf(self):
-        pass
+        dist = self.create_pdf(0, 1)
+        y = dist.log_pdf(1)
+        assert np.round(y, 2) == -1.42
+    
+    def test_log_negative(self):
+        dist = self.create_pdf(0, 1)
+        y = dist.log_pdf(-1)
+        assert np.round(y, 2) == -1.42
 
     def test_log_pdf_exception(self):
         dist = Normal()
