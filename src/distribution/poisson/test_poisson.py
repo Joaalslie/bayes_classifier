2d54ab3cbb5dbfcc80878d59de1b33f376956794
joaalslie <jal029@uit.no>
2021-01-21 13:10:39 +0100

    Add test cases for pdf() and log_pdf() functions

--- src/distribution/poisson/test_poisson.py
+++ src/distribution/poisson/test_poisson.py
@@ -1,25 +1,43 @@
+import numpy as np
 import unittest
 
 from .poisson import Poisson
 
 
 class TestPoisson(unittest.TestCase):
+    def create_pdf(self, alpha):
+        dist = Poisson()
+        # Set the parameter manually for simplicity
+        dist.alpha = alpha
+        dist.is_fit = True
+        return dist
+
     def test_pdf(self):
-        pass
+        dist = self.create_pdf(3)
+        y = dist.pdf(1)
+        # 2 Decimals should be sufficient
+        assert np.round(y, 2) == 0.15
 
-    def test_pdf_negative_value(self):
-        pass
+    def test_pdf_negative_value_exception(self):
+        # Ensure that Exception is raised when x is less than 0
+        dist = self.create_pdf(3)
+        self.assertRaises(Exception, dist.pdf, -1)
 
-    def test_pdf_exception(self):
+    def test_pdf_estimator_exception(self):
         dist = Poisson()
         # Call on pdf before training to invoke exception
         self.assertRaises(Exception, dist.pdf, None)
 
     def test_log_pdf(self):
-        pass
+        dist = self.create_pdf(3)
+        y = dist.log_pdf(1)
+        # 2 Decimals should be sufficient
+        assert np.round(y, 2) == -1.90
 
     def test_log_pdf_negative_value(self):
-        pass
+        # Ensure that Exception is raised when x is less than 0
+        dist = self.create_pdf(3)
+        self.assertRaises(Exception, dist.log_pdf, -1)
 
     def test_log_pdf_exception(self):
         dist = Poisson()
