637a648d9efd669d960105cd28ee4d0da87fd326
joaalslie <jal029@uit.no>
2021-01-21 11:07:48 +0100

    Fill tests for pdf and log_pdf

--- src/distribution/gamma/test_gamma.py
+++ src/distribution/gamma/test_gamma.py
@@ -1,11 +1,29 @@
+import numpy as np
 import unittest
 
 from .gamma import Gamma
 
 
 class TestGamma(unittest.TestCase):
+    def create_pdf(self, alpha, beta):
+        dist = Gamma()
+        # Set the parameters manually for simplicity
+        dist.alpha = alpha
+        dist.beta = beta
+        dist.is_fit = True
+        return dist
+
     def test_pdf(self):
-        pass
+        dist = self.create_pdf(2, 2)
+        y = dist.pdf(1)
+        # 2 decimals should be sufficient
+        assert np.round(y, 2) == 0.15
+    
+    def test_pdf_negative_x(self):
+        dist = self.create_pdf(2, 2)
+        y = dist.pdf(1)
+        # 2 decimals should be sufficient
+        assert np.round(y, 2) == 0.15
 
     def test_pdf_exception(self):
         dist = Gamma()
@@ -13,7 +31,10 @@ class TestGamma(unittest.TestCase):
         self.assertRaises(Exception, dist.log_pdf, None)
 
     def test_log_pdf(self):
-        pass
+        dist = self.create_pdf(2, 2)
+        y = dist.log_pdf(1)
+        # 2 decimals should be sufficient
+        assert np.round(y, 2) == -1.89
 
     def test_log_pdf_exception(self):
         dist = Gamma()
