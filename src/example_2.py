ba0628b4137b3c978dace400c08ebb3423631d64
joaalslie <jal029@uit.no>
2021-01-23 23:48:12 +0100

    Add empty program class and main-idiom for example programs

---
+++ src/example_2.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+from bayes_classifier import BayesClassifier
+import distribution as dist
+
+
+class Program():
+    """
+
+    """
+    def __init__(self):
+        pass
+    
+    def main(self):
+        """
+
+        """
+        pass
+
+
+if __name__ == "__main__":
+    Program()
