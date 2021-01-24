ed4410d7afec3da292368434997373737b3b6a08
joaalslie <jal029@uit.no>
2021-01-24 23:54:35 +0100

    Add tests for remove_class() method

--- src/bayes_classifier/test_bayes_classifier.py
+++ src/bayes_classifier/test_bayes_classifier.py
@@ -24,14 +24,6 @@ class TestBayesClassifier(unittest.TestCase):
         classifier.unset_log_pdf()
         assert classifier.use_log_pdf == False
 
-    def test_remove_class(self):
-        pass
-
-    def test_remove_class_exception(self):
-        # Ensure that exception is raised when trying to add distribution
-        # on an invalid idx in the remove_class() function.
-        pass
-
     def test_add_class(self):
         pass
 
@@ -44,6 +36,34 @@ class TestBayesClassifier(unittest.TestCase):
         # Ensure that exception is raised when trying to add invalid
         # class/distribution in the add_class() function.
         pass
+
+    def test_remove_class(self):
+        # Ensure that distribution is removed from list after remove_class()
+        # function has been called
+        classifier = BayesClassifier(2)
+        classifier.add_class(Normal(), 1)
+        classifier.remove_class(1)
+        assert classifier.distributions[1] == None
+    
+    def test_remove_class_counter(self):
+        # Ensure that counter of added classes is correct after remove_class()
+        # function has been called
+        classifier = BayesClassifier(2)
+        classifier.add_class(Normal(), 1)
+        classifier.remove_class(1)
+        assert classifier.added_classes == 0
+
+    def test_remove_class_exception_negative_value(self):
+        # Ensure that exception is raised when trying to remove class
+        # on negative invalid idx in the remove_class() function.
+        classifier = BayesClassifier(2)
+        self.assertRaises(Exception, classifier.remove_class, -1)
+
+    def test_remove_class_exception_exceeding_idx(self):
+        # Ensure that exception is raised when trying to remove class
+        # on negative invalid idx in the remove_class() function.
+        classifier = BayesClassifier(2)
+        self.assertRaises(Exception, classifier.remove_class, 2)
 
     def test_predict(self):
         pass
